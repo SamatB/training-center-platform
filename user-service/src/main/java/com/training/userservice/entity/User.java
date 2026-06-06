f8939f7c2eec0470ec593eba6395dfe5c4213453
Albert0002 <toktosunovalbert512@gmail.com>
2026-06-06 18:11:07 +0300

    Сделал created User Entity

---
+++ user-service/src/main/java/com/training/userservice/entity/User.java
@@ -0,0 +1,43 @@
+package com.training.userservice.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "id", nullable = false, updatable = false)
+    private UUID id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(name = "email", nullable = false,unique = true)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "status", nullable = false)
+    private UserStatus status;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "update_at", nullable = false)
+    private LocalDateTime updatedAt;
+}
---
+++ user-service/src/main/java/com/training/userservice/entity/UserStatus.java
@@ -0,0 +1,7 @@
+package com.training.userservice.entity;
+
+public enum UserStatus {
+    ACTIVE,
+    BLOCKED,
+    DELETED
+}
