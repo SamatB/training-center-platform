15db70f104b6f746346a5eb500a9ec8b879a13fe
Aiza <nazaraz88@mail.ru>
2026-07-02 08:04:07 +0600

    style: add @Builder to EnrollmentRequest for consistency with project

--- enrollment-service/src/main/java/com/training/enrollmentservice/dto/request/EnrollmentRequest.java
+++ enrollment-service/src/main/java/com/training/enrollmentservice/dto/request/EnrollmentRequest.java
@@ -1,15 +1,13 @@
 package com.training.enrollmentservice.dto.request;
 
 import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.UUID;
 
 @Getter
 @Setter
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class EnrollmentRequest {
