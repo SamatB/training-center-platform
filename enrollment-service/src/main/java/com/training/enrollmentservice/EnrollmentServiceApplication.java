6d934ee4bd1951eed038105803e1a77ff58e04b4
Aiza <nazaraz88@mail.ru>
2026-07-16 18:41:48 +0600

    Add Liquibase setup for enrollment service

--- enrollment-service/src/main/java/com/training/enrollmentservice/EnrollmentServiceApplication.java
+++ enrollment-service/src/main/java/com/training/enrollmentservice/EnrollmentServiceApplication.java
@@ -2,13 +2,11 @@ package com.training.enrollmentservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class EnrollmentServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EnrollmentServiceApplication.class, args);
     }
-
 }
