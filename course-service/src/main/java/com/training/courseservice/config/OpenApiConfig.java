2281ea17003df9ee7630d5502656bb7ca88f2f49
Albert0002 <toktosunovalbert512@gmail.com>
2026-08-05 22:31:11 +0300

    TCP-42 application.yml User Service

--- course-service/src/main/java/com/training/courseservice/config/OpenApiConfig.java
+++ course-service/src/main/java/com/training/courseservice/config/OpenApiConfig.java
@@ -3,12 +3,13 @@ package com.training.courseservice.config;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class OpenApiConfig {
 
     @Bean
     public OpenAPI courseServiceOpenAPI() {
-
         return new OpenAPI()
                 .info(new Info()
                         .title("Course Service API")
