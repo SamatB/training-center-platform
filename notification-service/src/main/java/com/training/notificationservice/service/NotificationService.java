ce891410df98c3d3257f146ef9d7468e1e1f54d1
Aiza <nazaraz88@mail.ru>
2026-08-08 03:09:00 +0600

    TCP-53 Add notification service

---
+++ notification-service/src/main/java/com/training/notificationservice/service/NotificationService.java
@@ -0,0 +1,48 @@
+package com.training.notificationservice.service;
+
+import com.training.notificationservice.dto.NotificationCreateRequest;
+import com.training.notificationservice.dto.NotificationResponse;
+import com.training.notificationservice.entity.Notification;
+import com.training.notificationservice.mapper.NotificationMapper;
+import com.training.notificationservice.repository.NotificationRepository;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@RequiredArgsConstructor
+@Service
+public class NotificationService {
+
+    private final NotificationRepository notificationRepository;
+    private final NotificationMapper notificationMapper;
+
+    @Transactional
+    public NotificationResponse create(NotificationCreateRequest request) {
+
+        Notification notification = notificationMapper.toEntity(request);
+
+        Notification saved = notificationRepository.save(notification);
+
+        return notificationMapper.toResponse(saved);
+    }
+
+    @Transactional(readOnly = true)
+    public NotificationResponse getById(UUID id) {
+
+        Optional<Notification> notification = notificationRepository.findById(id);
+
+        if (notification.isEmpty()) {
+            throw new EntityNotFoundException(
+                    "Запись не найдена с id: " + id
+            );
+        }
+
+        Notification foundNotification = notification.get();
+
+        return notificationMapper.toResponse(foundNotification);
+    }
+}
