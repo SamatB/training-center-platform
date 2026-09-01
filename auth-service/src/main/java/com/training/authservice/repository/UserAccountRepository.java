637e4c4b44db44ab0ac2841b378d401bdc67ec0c
Aiza <nazaraz88@mail.ru>
2026-09-02 00:57:01 +0600

    TCP-63 create UserAccountRepository

---
+++ auth-service/src/main/java/com/training/authservice/repository/UserAccountRepository.java
@@ -0,0 +1,17 @@
+package com.training.authservice.repository;
+import com.training.authservice.entity.UserAccount;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface UserAccountRepository extends JpaRepository<UserAccount,UUID> {
+
+    Optional<UserAccount> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+
+    Optional<UserAccount> findByEmailAndEnabledTrue(String email);
+
+}
